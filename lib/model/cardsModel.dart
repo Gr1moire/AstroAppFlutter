901bde178cadc6893ff3a244a0dc64886d4ca1b0
Pascal Betti <pascal.betti@epitech.eu>
2019-12-19 11:09:25 +0100

    positionText order is now Passé, Présent, Futur

--- lib/model/cardsModel.dart
+++ lib/model/cardsModel.dart
@@ -1,54 +1,54 @@
 import 'package:flutter/material.dart';
 
-class Cards{
+class Cards {
   GlobalKey _titleKey = GlobalKey();
-  
+
   final List<String> arcanaNames = [
-      "Le Tronc",
-      "La Balance",
-      "La Tour",
-      "L'Aiguière",
-      "La Flèche",
-      "L'Epieu",
-      "Le Tronc renversé",
-      "La Balance renversée",
-      "La Tour renversée",
-      "L'Aiguière renversée",
-      "La Flèche renversée",
-      "L'Epieu renversé",
-    ];
+    "Le Tronc",
+    "La Balance",
+    "La Tour",
+    "L'Aiguière",
+    "La Flèche",
+    "L'Epieu",
+    "Le Tronc renversé",
+    "La Balance renversée",
+    "La Tour renversée",
+    "L'Aiguière renversée",
+    "La Flèche renversée",
+    "L'Epieu renversé",
+  ];
 
-    final List<String> arcanaSymbols = [
-      "Protection & générosité",
-      "Justice & prospérité",
-      "Ingéniosité & volonté",
-      "Savoir & éveil",
-      "Liberté & découverte",
-      "Force & foi",
-      "Reconsidération & maladie",
-      "Jugement & trahison",
-      "Difficulté & persévérance",
-      "Egarement de l'âme & introspection",
-      "Obstination & jalousie",
-      "Epreuve & vengeance",
-    ];
+  final List<String> arcanaSymbols = [
+    "Protection & générosité",
+    "Justice & prospérité",
+    "Ingéniosité & volonté",
+    "Savoir & éveil",
+    "Liberté & découverte",
+    "Force & foi",
+    "Reconsidération & maladie",
+    "Jugement & trahison",
+    "Difficulté & persévérance",
+    "Egarement de l'âme & introspection",
+    "Obstination & jalousie",
+    "Epreuve & vengeance",
+  ];
 
-    final List<String> arcanaMainColors = [
-      "edf982",
-      "ffc0af",
-      "ffd7af",
-      "8cafc0",
-      "8dc6b3",
-      "95a1c7",
-      "edf982",
-      "ffc0af",
-      "ffd7af",
-      "8cafc0",
-      "8dc6b3",
-      "95a1c7",
-    ];
+  final List<String> arcanaMainColors = [
+    "edf982",
+    "ffc0af",
+    "ffd7af",
+    "8cafc0",
+    "8dc6b3",
+    "95a1c7",
+    "edf982",
+    "ffc0af",
+    "ffd7af",
+    "8cafc0",
+    "8dc6b3",
+    "95a1c7",
+  ];
 
-  final List<AssetImage> cardsList = [
+  final List<AssetImage> arcanaImages = [
     AssetImage('assets/cards/Bole.png'),
     AssetImage('assets/cards/Balance.png'),
     AssetImage('assets/cards/Spire.png'),
@@ -66,25 +66,24 @@ class Cards{
   final AssetImage cardBack = AssetImage('assets/cards/Back.png');
   final List<String> positionText = [
     "Passé",
-    "Futur",
     "Présent",
+    "Futur",
   ];
 
-    double getTitlePosition(double screenHeight) {
+  double getTitlePosition(double screenHeight) {
     final RenderBox renderBoxTitle =
         _titleKey.currentContext.findRenderObject();
     final positionTitle = renderBoxTitle.localToGlobal(Offset.zero);
     return screenHeight - (positionTitle.dy * 1.5);
   }
 
-    // TODO: Add color by arcana
-  Widget displayArcanaName(List<List<int>> drawnCards, int lastCardDrawn, num activePage, bool isVisible) {
+  // TODO: Add color by arcana
+  Widget displayArcanaName(List<List<int>> drawnCards, int lastCardDrawn,
+      num activePage, bool isVisible) {
     String arcanaNameText = ' ';
-    // ! There certainly is a better way to do that.
     if (drawnCards[activePage][0] != -7) {
       arcanaNameText = this.arcanaNames[drawnCards[activePage][0]];
-    } else if (!(drawnCards[activePage][0] == -7) &&
-        lastCardDrawn >= 0)
+    } else if (!(drawnCards[activePage][0] == -7) && lastCardDrawn >= 0)
       arcanaNameText = this.arcanaNames[lastCardDrawn];
     else
       arcanaNameText = ' ';
@@ -114,16 +113,14 @@ class Cards{
     );
   }
 
-  Widget displayArcanaSymbols(List<List<int>> drawnCards, int lastCardDrawn, num activePage, bool isVisible) {
+  Widget displayArcanaSymbols(List<List<int>> drawnCards, int lastCardDrawn,
+      num activePage, bool isVisible) {
     String arcanaSymbolsText = 'Ceci est caché';
     if (drawnCards[activePage][0] != -7) {
-      arcanaSymbolsText = "« " +
-          this.arcanaSymbols[drawnCards[activePage][0]] +
-          " »";
-    } else if (!(drawnCards[activePage][0] == -7) &&
-        lastCardDrawn >= 0)
       arcanaSymbolsText =
-          "« " + this.arcanaSymbols[lastCardDrawn] + " »";
+          "« " + this.arcanaSymbols[drawnCards[activePage][0]] + " »";
+    } else if (!(drawnCards[activePage][0] == -7) && lastCardDrawn >= 0)
+      arcanaSymbolsText = "« " + this.arcanaSymbols[lastCardDrawn] + " »";
     else
       arcanaSymbolsText = ' ';
 
